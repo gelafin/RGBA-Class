b4d3b1930d830eeeeb1c0368ac32453c91a77cd7
gelafin <46856925+gelafin@users.noreply.github.com>
2019-04-06 09:28:23 -0700

    Add files via upload

---
+++ RGBA Class.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdint>
+
+class RGBA{
+private:
+  std::uint_fast8_t m_red;
+  std::uint_fast8_t m_green;
+  std::uint_fast8_t m_blue;
+  std::uint_fast8_t m_alpha;
+
+public:
+  RGBA(std::uint_fast8_t red = 0, std::uint_fast8_t green = 0, std::uint_fast8_t blue = 0, std::uint_fast8_t alpha = 255)
+  : m_red(red), m_green(green), m_blue(blue), m_alpha(alpha){
+  }
+  void print(){
+    std::cout << "r=" << static_cast<int>(m_red) << "\n";
+    std::cout << "g=" << static_cast<int>(m_green) << "\n";
+    std::cout << "b=" << static_cast<int>(m_blue) << "\n";
+    std::cout << "a=" << static_cast<int>(m_alpha) << "\n";
+  }
+};
+
+int main(){
+
+RGBA teal(0, 127, 127);
+teal.print();
+
+int sillyViewerVariable;
+std::cin >> sillyViewerVariable;
+return 0;
+}
